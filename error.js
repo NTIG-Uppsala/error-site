280becb170d75530925f59b5aaf4d7fe4171c995
Leo Ahlinder <59998695+LeoAhlinder@users.noreply.github.com>
2023-11-24 10:41:13 +0100

    Update error.js

--- error.js
+++ error.js
@@ -1,22 +1,83 @@
-const axios = require('axios');
-const exec = require('child_process').exec;
+const axios = require("axios");
+const { exec } = require("child_process");
+const fs = require("fs");
 
-const url = 'https://narvaro.ntig.dev/';
+const url = "https://narvaro.ntig.dev/";
 const payload = {};
 const headers = {};
 
-axios.post(url, payload, { headers })
-    .then(response => {
-        const errorSite = 'http://127.0.0.1:5500/index.html';
+const displayCommand = "export DISPLAY=:0.0";
 
-        if (response.status == 200) {
-            // opening the site
-            console.log('Status code:', response.status);
-            exec('start chrome ' + errorSite);
-        } else {
-            console.log("errorrrrrrrrrrrr");
+let siteOpen = fs.existsSync("siteOpen.txt")
+  ? fs.readFileSync("siteOpen.txt", "utf8") === "true"
+  : false;
+
+let serverSiteOpen = fs.existsSync("serverSiteOpen.txt")
+  ? fs.readFileSync("serverSiteOpen.txt", "utf8") === "true"
+  : false;
+
+axios
+  .post(url, payload, { headers })
+  .then((response) => {
+    console.log(response.status, siteOpen, serverSiteOpen);
+    const errorSite = "192.168.203.143";
+
+    if (response.status != 200 && siteOpen === false) {
+      // Opening the site with the export DISPLAY command
+      console.log("Status code:", response.status);
+      siteOpen = true;
+      fs.writeFileSync("siteOpen.txt", "true");
+
+      exec(
+        `${displayCommand} && chromium-browser ${errorSite} --kiosk`,
+        (error, stdout, stderr) => {
+          if (error) {
+            console.log(`${error}`);
+            return;
+          } else {
+            console.log(stdout, stderr);
+          }
         }
-    })
-    .catch(error => {
-        console.log('Error:', error);
-    });
+      );
+      console.log(errorSite);
+    } else if (response.status != 200 && siteOpen === true) {
+      console.log("Error");
+    } else if (response.status === 200 && serverSiteOpen === false) {
+      console.log("server up and running");
+      exec(
+        `${displayCommand} && chromium-browser ${url} --kiosk`,
+        (error, stdout, stderr) => {
+          if (error) {
+            console.log(`${error}`);
+            return;
+          } else {
+            console.log(stdout, stderr);
+            serverSiteOpen = true;
+            fs.writeFileSync("serverSiteOpen.txt", "true");
+            siteOpen = false;
+            fs.writeFileSync("siteOpen.txt", "false");
+          }
+        }
+      );
+    }
+  })
+  .catch((error) => {
+    console.log("Error with server");
+    if (!siteOpen) {
+      // Close existing Chromium instances and open a new one in kiosk mode
+      exec(
+        `${displayCommand} && killall chromium-browser && chromium-browser 192.168.203.143 --kiosk`,
+        (chromiumError, stdout, stderr) => {
+          if (chromiumError) {
+            console.error(`Error closing/opening Chromium: ${chromiumError}`);
+            return;
+          }
+          console.log("Chromium closed/opened successfully");
+          siteOpen = true;
+          fs.writeFileSync("siteOpen.txt", "true");
+          serverSiteOpen = false;
+          fs.writeFileSync("serverSiteOpen.txt", "false");
+        }
+      );
+    }
+  });
